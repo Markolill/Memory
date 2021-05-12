bfc6dcef8c3ab153eed926d139f36f66692076aa
marko <markolill@hotmail.com>
2021-05-12 22:50:29 +0300

    normaalse MENÜÜGA

--- src/main/java/oop/Memory.java
+++ src/main/java/oop/Memory.java
@@ -24,19 +24,26 @@ import javafx.util.Duration;
 import java.util.Arrays;
 
 public class Memory extends Application {
+    private int raskusaste;
 
     @Override
     public void start(Stage peaLava) throws Exception {
+        peaLava.setScene(algus(peaLava));
+        peaLava.show();
+    }
+
+
+    public Scene algus(Stage peaLava) throws Exception {
         GridPane gridPane = new GridPane();
         Font font = Font.font("Arial", FontWeight.BOLD, 15);
-        Text taane = new Text();
+        Text taane=new Text();
         taane.setText("");
-        gridPane.add(taane, 0, 0);
+        gridPane.add(taane,0,0);
 
         Text tekst = new Text();
         tekst.setFont(font);
         tekst.setText(" Vali mängu raskus");
-        gridPane.add(tekst, 1, 1);
+        gridPane.add(tekst,1,1);
 
 
         Button nupp = new Button("Mängima");
@@ -55,70 +62,72 @@ public class Memory extends Application {
 
 
         Scene scene = new Scene(gridPane, 250, 150);
-        peaLava.setScene(scene);  // lavale lisatakse stseen
-        peaLava.show();  // lava tehakse nähtavaks
-
-
-
-
 
         nupp.setOnAction(new EventHandler<>() {
             @Override
             public void handle(ActionEvent e) {
-                int raskusaste = (int) slider.getValue();
-
-                int suurus = raskusaste*2;
-                Mängulaud laud = new Mängulaud(suurus);
-                GridPane gridPane = new GridPane();
-                Kontroll kontroll = new Kontroll(laud);
-                BorderPane border = new BorderPane();
-                Text kell = new Text("0");
-                Kell aeg = new Kell();
-
-                aeg.startstopp(kell);
-
-                Font font = Font.font("Arial", FontWeight.BOLD, 15);
-
-                for (int i = 0; i < suurus; i++) {
-                    for (int j = 0; j < suurus; j++) {
-                        PauseTransition wait = new PauseTransition(Duration.seconds(3));
-                        Button nupp = new Button(laud.getElement(new int[]{i, j}));
-
-                        nupp.setStyle("-fx-background-color: gray;");
-                        nupp.setMinSize(40, 40);
-                        nupp.setFont(font);
-
-                        gridPane.add(nupp, i, j);
-
-
-                        wait.setOnFinished(event -> nupp.setText(""));
-                        wait.play();
-
-                        int veerg = GridPane.getColumnIndex(nupp);
-                        int rida = GridPane.getRowIndex(nupp);
-
-                        nupp.setOnMouseClicked(event -> {
-                            try {
-                                kontroll.lisa(nupp, rida, veerg);
-                            } catch (InterruptedException g) {
-                                g.printStackTrace();
-                                System.out.println("viga");
-                            }
-                        });
-                    }
+                raskusaste = (int) slider.getValue();
+                try {
+                    peaLava.setScene(mäng(peaLava));
+                } catch (Exception exception) {
+                    exception.printStackTrace();
                 }
-                gridPane.setHgap(10);
-                gridPane.setVgap(10);
-
-                border.setLeft(kell);
-                border.setCenter(gridPane);
-
-                Scene scene = new Scene(border, 500, 500);
-                peaLava.setScene(scene);  // lavale lisatakse stseen
-                peaLava.show();  // lava tehakse nähtavaks
-
             }
         });
+        return scene;
+
+    }
+
+
+    public Scene mäng(Stage peaLava) throws Exception {
+        int suurus = raskusaste*2;
+        Mängulaud laud = new Mängulaud(suurus);
+        GridPane gridPane = new GridPane();
+        Kontroll kontroll = new Kontroll(laud);
+        BorderPane border = new BorderPane();
+        Text kell = new Text("0");
+        Kell aeg = new Kell();
+
+        aeg.startstopp(kell);
+
+        Font font = Font.font("Arial", FontWeight.BOLD, 15);
+
+        for (int i = 0; i < suurus; i++) {
+            for (int j = 0; j < suurus; j++) {
+                PauseTransition wait = new PauseTransition(Duration.seconds(3));
+                Button nupp = new Button(laud.getElement(new int[]{i, j}));
+
+                nupp.setStyle("-fx-background-color: gray;");
+                nupp.setMinSize(40, 40);
+                nupp.setFont(font);
+
+                gridPane.add(nupp, i, j);
+
+
+                wait.setOnFinished(event -> nupp.setText(""));
+                wait.play();
+
+                int veerg = GridPane.getColumnIndex(nupp);
+                int rida = GridPane.getRowIndex(nupp);
+
+                nupp.setOnMouseClicked(event -> {
+                    try {
+                        kontroll.lisa(nupp, rida, veerg);
+                    } catch (InterruptedException g) {
+                        g.printStackTrace();
+                        System.out.println("viga");
+                    }
+                });
+            }
+        }
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+
+        border.setLeft(kell);
+        border.setCenter(gridPane);
+
+        Scene scene = new Scene(border, 500, 500);
+        return scene;
 
     }
 
